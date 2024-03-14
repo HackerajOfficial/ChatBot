a8395dc76eb68e62ce429b6c7d5c04dbf044c420
HackerajOfficial <Hackeraj.np@hotmail.com>
2024-03-14 06:44:04 +0545

    Send userinput to backend and return to frontend

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from blog import views
 
 urlpatterns = [
     path('', views.blog, name='blog'),
+    path('getResponse', views.getResponse, name='getResponse'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 
 def blog(request):
     return render(request, template_name='blog.html')
+
+def getResponse(request):
+    userMessage = request.GET.get('userMessage')
+    print(userMessage)
+    response = userMessage
+    return HttpResponse(response)
