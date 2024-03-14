5f87062634aac635f3cafe5cc3ac6171822bdbc6
HackerajOfficial <Hackeraj.np@hotmail.com>
2024-03-14 07:04:14 +0545

    Create UserInfo model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class UserInfo(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(null=True)
+    mobile_number = models.CharField(max_length=15, null=True)
+    address = models.CharField(max_length=255, null=True)
+
+    def __str__(self):
+        return self.name
